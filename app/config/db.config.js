38e3efae5e42c30d9a493dc6ec6ae7ec7479f120
kj1999 <44728002+kj1999@users.noreply.github.com>
2023-06-21 16:52:48 +0530

    db changes

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "cybage@123",
+    HOST: "aws.connect.psdb.cloud",
+    USER: "w29ntpye2mfgh78acrgd",
+    PASSWORD: "pscale_pw_luAyOKPSV72RQuqwDMIXCkcuTwIPifulPbsmzCHlnat",
     DB: "food_delivery",
     dialect: "mysql",
     pool: {
@@ -9,5 +9,10 @@ module.exports = {
       min: 0,
       acquire: 30000,
       idle: 10000
-    }
+    },
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: true,
+      },
+    },
   };
