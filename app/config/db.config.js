023ddd4ef18df9891addf4baba6f2880a5446263
kj1999 <44728002+kj1999@users.noreply.github.com>
2023-09-03 17:10:54 +0530

    config changes

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -13,9 +13,5 @@ module.exports = {
       acquire: 30000,
       idle: 10000
     },
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: true,
-      },
-    },
+    dialectOptions: {},
   };
--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.use((err, req, res, next) => {
   res.status(status);
   res.render('error');
 });
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8082;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
